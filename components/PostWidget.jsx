56df447de9464871660275dcfbf92a6b093c113f
Eleni Danessis <e.danessis@gmail.com>
2022-11-27 20:06:55 -0500

    init

--- components/PostWidget.jsx
+++ components/PostWidget.jsx
@@ -1,10 +1,22 @@
 import React from "react";
+import Link from 'next/link';
 
-const PostWidget = () => {
+const PostWidget = ({post}) => {
   return (
-    <div>
-      <div className="">
-        <h1 className="">Latest Post Widget</h1>
+    <div className="top-latest-post-widget2">
+      <div className="latest-post-widget">
+        <div className="post-text-container">
+          {/* Use tailwind here for conditional css */}
+          <div className="container">
+            <div className="img1">
+              <img className="img-widget" src={post.html} />
+            </div>
+            <div className="texts2">
+              <p className="category-text">{post.category}</p>
+              <h2 className="post-title">{post.title}</h2>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
