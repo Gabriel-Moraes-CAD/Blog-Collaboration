1fab76a3ff6dd2245ae85b324d73c1f5f0d58cba
Gabriel <mr.gabriel.moraes@gmail.com>
2022-12-03 18:10:34 -0500

    Graphql Changes

--- components/LifeStyle.jsx
+++ components/LifeStyle.jsx
@@ -1,14 +1,22 @@
 import React from "react";
 
-const LifeStyle = ({ post }) => {
+const LifeStyle = ({
+  title,
+  datePublished,
+  author,
+  coverPhoto,
+  content,
+  category,
+  text,
+}) => {
   return (
     <div className="lifestyle-top-post ">
-      <img className="img-lifestyle" src={post.html} />
+      <img className="img-lifestyle" src={coverPhoto?.url} />
       <div className="post-text">
         {/* Use tailwind here for conditional css */}
-        <p className="category-text">{post.category}</p>
-        <h2 className="post-title">{post.title}</h2>
-        <p className="short-desc">{post.excerpt}</p>
+        <p className="category-text">{category}</p>
+        <h2 className="post-title">{title}</h2>
+        <p className="short-desc">{content.text.substring(0, 75)}</p>
       </div>
     </div>
   );
