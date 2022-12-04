e81920c3cafa836d31ba4d4aa40b1823f18f6e3e
Gabriel <mr.gabriel.moraes@gmail.com>
2022-12-04 11:32:38 -0500

    Graphql + Css Cond + Slug

--- components/Trending.jsx
+++ components/Trending.jsx
@@ -1,15 +1,34 @@
 import React from "react";
+import Link from "next/link";
 
-const Trending = ({ post }) => {
+const Trending = ({
+  title,
+  datePublished,
+  author,
+  coverPhoto,
+  content,
+  category,
+  text,
+  slug,
+}) => {
   return (
     <div className="trending-top-post ">
-      <img className="img-trending" src={post.html} />
-      <div className="post-text">
-        {/* Use tailwind here for conditional css */}
-        <p className="category-text">{post.category}</p>
-        <h2 className="post-title">{post.title}</h2>
-        <p className="short-desc">{post.excerpt}</p>
-      </div>
+      <Link href={"/posts/" + slug}>
+        <img className="img-trending" src={coverPhoto?.url} />
+        <div className="post-text">
+          <p
+            className={`${category === "Trails" ? "Trails" : ""} 
+        ${category === "Outdoors" ? "Outdoors" : ""}
+        ${category === "Recipes" ? "Recipes" : ""}
+        ${category === "LifeStyle" ? "LifeStyle" : ""}
+        ${category === "Winter" ? "Winter" : ""}`}
+          >
+            {category}
+          </p>{" "}
+          <h2 className="post-title">{title}</h2>
+          <p className="short-desc">{content?.text.substring(0, 75)}</p>
+        </div>
+      </Link>
     </div>
   );
 };
